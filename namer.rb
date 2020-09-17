c13c6c6be4d3bf122b9cfd6541bd3580d8d4e98e
Alex Golshani <4546371+xitonix@users.noreply.github.com>
2020-09-18 09:42:57 +1000

    V49

--- namer.rb
+++ namer.rb
@@ -2,7 +2,7 @@ class Namer < Formula
     desc "A CLI tool for naming"
     homepage "https://github.com/xitonix/namer"
     url "https://github.com/xitonix/namer/releases/download/v0.0.49/namer_0.0.49_darwin_amd64.tar.gz"
-  
+    sha256 "8ff12d21f5dddf2b221d3492cc1f2f4304d4ca45b92dc2a586028edc6d66b081"
     def install
       bin.install "namer"
     end
