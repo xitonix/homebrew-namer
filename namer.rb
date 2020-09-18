5fafe6922b91b4110475bea85cd053e35cdf367c
Alex Golshani <4546371+xitonix@users.noreply.github.com>
2020-09-18 11:13:51 +1000

    v49

--- namer.rb
+++ namer.rb
@@ -3,6 +3,7 @@ class Namer < Formula
     homepage "https://github.com/xitonix/namer"
     url "https://github.com/xitonix/namer/releases/download/v0.0.49/namer_0.0.49_darwin_amd64.tar.gz"
     sha256 "8ff12d21f5dddf2b221d3492cc1f2f4304d4ca45b92dc2a586028edc6d66b081"
+    version "v0.0.49"
     def install
       bin.install "namer"
     end
